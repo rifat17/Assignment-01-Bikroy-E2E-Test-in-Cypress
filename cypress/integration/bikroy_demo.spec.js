b879cc072ace369f028b9e1e2749aeaa5bb2010e
Abdullah Al Hasib <a.hasib.rifat@gmail.com>
2021-06-16 13:18:06 +0600

    added allure report and result

--- cypress/integration/bikroy_demo.spec.js
+++ cypress/integration/bikroy_demo.spec.js
@@ -5,21 +5,24 @@ const chance = new Chance();
 
 describe("Testing Bikroy.com", () => {
   beforeEach(function () {
-    cy.clearCookies();
-    // cy.visit("https://bikroy.com/en");
+    cy.visit("https://bikroy.com/en");
     cy.on("uncaught:exception", (err, runnable) => {
       return false;
     });
   });
 
-  it.skip("TC-001: Open Bikroy Site in Browser", () => {
+  afterEach(function () {
+    cy.clearCookies();
+  });
+
+  it("TC-001: Open Bikroy Site in Browser", () => {
     cy.title().should(
       "eq",
       "Bikroy.com - Electronics, Cars, Property and Jobs in Bangladesh"
     );
   });
 
-  it.skip("TC-003 Login", () => {
+  it("TC-003 Login", () => {
     cy.contains("Log in").click();
     cy.contains("Continue with Email").click();
     cy.get("button").contains("Login").should("be.disabled");
@@ -33,7 +36,7 @@ describe("Testing Bikroy.com", () => {
     cy.clearCookies();
   });
 
-  it.skip("TC-004 Invalid Password", () => {
+  it("TC-004 Invalid Password", () => {
     cy.contains("Log in").click();
     cy.contains("Continue with Email").click();
     cy.get("button").contains("Login").should("be.disabled");
@@ -56,8 +59,8 @@ describe("Testing Bikroy.com", () => {
     // );
   });
 
-  it.skip("TC-005 Show all ads", { defaultCommandTimeout: 10000 }, () => {
-    cy.visit("https://bikroy.com/en");
+  it("TC-005 Show all ads", { defaultCommandTimeout: 10000 }, () => {
+    // cy.visit("https://bikroy.com/en");
     cy.get(
       ".ui-nav-item.force-show-label.browse-ads.gtm-hamburger-ads"
     ).click();
@@ -65,7 +68,7 @@ describe("Testing Bikroy.com", () => {
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-006 Show all urgent ads", () => {
+  it("TC-006 Show all urgent ads", () => {
     cy.visit("https://bikroy.com/en/ads");
 
     cy.get(
@@ -75,31 +78,31 @@ describe("Testing Bikroy.com", () => {
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-007 Display ads by Category", () => {
+  it("TC-007 Display ads by Category", () => {
     cy.visit("https://bikroy.com/en/ads");
     cy.get(".list-item--2dM7Z").contains("Mobile").should("be.visible").click();
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-008 Show ads by location", () => {
+  it("TC-008 Show ads by location", () => {
     cy.visit("https://bikroy.com/en/ads");
     cy.get(".list-item--2dM7Z").contains("Dhaka").should("be.visible").click();
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-009 Ads Search", () => {
+  it("TC-009 Ads Search", () => {
     cy.visit("https://bikroy.com/en/ads/dhaka");
     cy.get("input[type=search]").clear().type("Dell Laptop").type("{enter}");
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-010 Search and count number of ads", () => {
-    cy.visit("https://bikroy.com/en/ads/");
+  it("TC-010 Search and count number of ads", () => {
+    cy.visit("https://bikroy.com/en/ads");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
     cy.get("ul[data-testid=list]>li").should("have.length.gt", 0);
   });
 
-  it.skip("TC-011 Get lowest price", () => {
+  it("TC-011 Get lowest price", () => {
     cy.visit("https://bikroy.com/en/ads/");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
     cy.get("ul[data-testid=list]>li").should(($elements) => {
@@ -111,7 +114,7 @@ describe("Testing Bikroy.com", () => {
     });
   });
 
-  it.skip("TC-013 Show to product details", () => {
+  it("TC-013 Show to product details", () => {
     cy.visit("https://bikroy.com/en/ads/");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
     cy.wait(1000);
@@ -123,7 +126,7 @@ describe("Testing Bikroy.com", () => {
     cy.get(".title--3s1R8").should("be.visible").should("have.length.gt", 0);
   });
 
-  it.skip("TC-014 Show phone number", () => {
+  it("TC-014 Show phone number", () => {
     cy.visit("https://bikroy.com/en/ads/");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
     cy.wait(1000);
@@ -143,7 +146,7 @@ describe("Testing Bikroy.com", () => {
     });
   });
 
-  it.skip("TC-015 Assert product description", () => {
+  it("TC-015 Assert product description", () => {
     cy.visit("https://bikroy.com/en/ads/");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
     cy.wait(1000);
@@ -159,7 +162,7 @@ describe("Testing Bikroy.com", () => {
       .should("have.length.gt", 0);
   });
 
-  it.skip("TC-018 Add product on Favorites", () => {
+  it("TC-018 Add product on Favorites", () => {
     cy.login("lowaci7232@threepp.com", "bikroy123456");
     cy.visit("https://bikroy.com/en/ads/");
     cy.get("input[type=search]").clear().type("Dell Laptop{enter}");
@@ -172,15 +175,15 @@ describe("Testing Bikroy.com", () => {
     cy.get(".cta--3cXbe > :nth-child(2) > .btn--1gFez").click();
   });
 
-  it.skip("TC-019 Show to favorites product list", () => {
+  it("TC-019 Show to favorites product list", () => {
     cy.login("lowaci7232@threepp.com", "bikroy123456");
     cy.get(".ui-nav-item.nav-dashboard").click();
     cy.get(".ui-nav-stack > ul > :nth-child(3) > a").click();
     cy.title().should("eq", "My Favorites - Bikroy.com");
   });
 
-  it.skip("TC-020 Assert Browse our top category", () => {
-    cy.visit("https://bikroy.com/en");
+  it("TC-020 Assert Browse our top category", () => {
+    // cy.visit("https://bikroy.com/en");
     cy.get(".home-focus > .title").should(($e) => {
       console.log($e[0].innerText);
       expect($e[0].innerText).to.be.eq("Browse our top categories:");
@@ -188,12 +191,15 @@ describe("Testing Bikroy.com", () => {
   });
   // cy.request(link.prop("href")).its(status).should("eq", 200);
   it("TC-021 Open all Browse our top category link", () => {
-    cy.visit("https://bikroy.com/en");
+    // cy.visit("https://bikroy.com/en");
     cy.get(".home-focus > .home-categories")
       .find("a")
       .each(($link) => {
         console.log($link);
-        cy.request($link.prop("href")).its("status").should("eq", 200);
+        cy.visit($link.prop("href"))
+          .url()
+          .should("include", $link.prop("href"));
+        // cy.request($link.prop("href")).its("status").should("eq", 200);
       });
   });
 });
